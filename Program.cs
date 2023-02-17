1ef8a8a17b2b189ec7a3ae6c05c69fd2f8437421
Davit Movsisyan <davitmovsisyan44@gmail.com>
2023-02-17 21:33:09 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Cht;
+ // пространство имен класса ChatHub
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();      // подключема сервисы SignalR
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");   // ChatHub будет обрабатывать запросы по пути /chat
+
+app.Run();
